cfff1e52f32978f98bf192e7b13491a8617c92a6
zyn334455 <kevinzhang334455@gmail.com>
2017-05-25 01:38:13 -0700

    commit

--- firstMissingPositive.cpp
+++ firstMissingPositive.cpp
@@ -4,42 +4,42 @@ using std::vector;
 
 void print_vec(const std::vector<int>& vec)
 {
-	for (const auto& num : vec)
-		std::cout << num << ' ';
+  for (const auto& num : vec)
+    std::cout << num << ' ';
 
-	std::cout << std::endl;
+  std::cout << std::endl;
 }
 
 class Solution {
 public:
-	int firstMissingPositive(vector<int>& nums)
-	{
-		if (nums.empty()) return 1;
-		for (long i = 0; i < nums.size(); i++)
-		{
-			while (nums[i] != i + 1 && nums[nums[i] - 1] != nums[i] 
-				&& nums[i] >= 1 && nums[i] <= nums.size())
-			{
-				std::swap(nums[i], nums[nums[i] - 1]);
-				print_vec(nums);
-			}
-		}
+  int firstMissingPositive(vector<int>& nums)
+  {
+    if (nums.empty()) return 1;
+    for (long i = 0; i < nums.size(); i++)
+    {
+      while (nums[i] != i + 1 && nums[nums[i] - 1] != nums[i] 
+        && nums[i] >= 1 && nums[i] <= nums.size())
+      {
+        std::swap(nums[i], nums[nums[i] - 1]);
+        print_vec(nums);
+      }
+    }
 
-		long i;
-		for (i = 0; i < nums.size(); i++)
-		{
-			if (nums[i] != i + 1)
-				break;
-		}
+    long i;
+    for (i = 0; i < nums.size(); i++)
+    {
+      if (nums[i] != i + 1)
+        break;
+    }
 
-		return i + 1;
-	}
+    return i + 1;
+  }
 };
 
 int main ()
 {
-	vector<int> case1 = {3, 4, -1, 1};
-	Solution s;
-	std::cout << s.firstMissingPositive(case1) << std::endl;
-	return 0;
+  vector<int> case1 = {3, 4, -1, 1};
+  Solution s;
+  std::cout << s.firstMissingPositive(case1) << std::endl;
+  return 0;
 }
