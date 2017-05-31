02d367f2f18744c550dedb43daa5dc5a4eaf7d67
zyn334455 <kevinzhang334455@gmail.com>
2017-05-30 23:52:41 -0700

    add test cases for permu

--- nextPermutation.cpp
+++ nextPermutation.cpp
@@ -20,7 +20,7 @@ public:
         
     long change = pivot;
     while (change != nums.size() - 1 && nums[change + 1] > nums[pivot - 1])
-        change++;
+      change++;
     
     std::swap(nums[pivot - 1], nums[change]);
     std::reverse(pivot + nums.begin(), nums.end());
@@ -28,7 +28,18 @@ public:
     for (long i = pivot; i < nums.size() - 1; i++)
     {
         if (nums[i] > nums[i + 1])
-            std::swap(nums[i], nums[i + 1]);
+          std::swap(nums[i], nums[i + 1]);
     }
   }
+}
+
+// Test cases
+int main ()
+{
+  vector<int> case1 = {1,2,4,3};
+  vector<int> case2 = {1,3,4,2};
+  vector<int> case3 = {2,1,4,5,3};
+  vector<int> case4 = {2,1,5,4,3};
+  vector<int> case5 = {1,5,1};
+  return 0;
 }
